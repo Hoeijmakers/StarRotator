6e82146569acbd61a1a4e5129b1908f19a6919b8
JVSeidel <julia.seidel@unige.ch>
2019-10-31 19:53:54 +0100

    added comments. calc_stellar_velocity not tested yet.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ######################
-#
-#tbd
+#authors: Jens Hoeijmakers and Julia Seidel
+#Description: Calculates the stellar spectrum
 #
 #
 #####################
@@ -22,32 +22,39 @@ def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate, proj_obliquity):
     based on Cegla+2016. See Fig. 3 and equations 2 - 8
     https://arxiv.org/pdf/1602.00322.pdf
     It takes the stellar parameters and then calculates the stellar velocities in all bins for one quarter of the stellar disk
-    input: x, y: vectors to create the stellar grid in units of stellar radius
+    input: x, y: 1D numpy arrays to create the stellar grid in units of stellar radius
+           i_stellar: inclination in degrees
+           vel_eq: equatorial stellar velocity
+           diff_rot_rate: differential rotation rate
+           proj_obliquity: projected obliquity
+    output: vel_stellar_grid: 2D numpy array of stellar velocities over one quarter of the stellar disk
     """
     #careful! all angles have to be in radiant!
     #Think carefully about which ones of these have to be transposed
-    #x_sq = np.einsum('i,j->ij',x,x)
-    #y_sq = np.einsum('i,j->ij',y,y)
+
+    #convert angles to rad
     alpha = convert_angle_to_rad(proj_obliquity)
     beta = convert_angle_to_rad(i_stellar)
+    #pre calculate matrices
     xy = np.einsum('i,j->ij',x,y)
     x_full = np.tile(x,(len(x),1))
     y_full = np.tile(y,(len(y),1)).T
     x_sq = x_full*x_full
     y_sq=y_full*y_full
+    
+    #this is the z coordinate in the tilted coordinate system from Cegla+2016
+    # with some trigonometry magic
     z = np.sqrt(1.-x_sq-y_sq+2.*x_full*y_full*np.sin(2.*alpha))
     #I have to add the stellar radius back in here somewhere
+    #equation 8 from Cegla+2016
     vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta*(1-diff_rot_rate*z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
     
     return vel_stellar_grid
-#maybe some global variables here
-
-
-if __name__ == '__main__':
 
 #main body of code
+if __name__ == '__main__':
 
-#call parser
+    #call parser, gets the input parameters from the user
     try:
         parser = argparse.ArgumentParser(description='Input variables for StarRotator:')
         parser.add_argument('wave_start', metavar='waveS', type=float,
@@ -72,8 +79,11 @@ if __name__ == '__main__':
     #for testing
     drr = 1.
     pob = 80.
+    
+    #two arrays for the x and y axis
     x = np.linspace(0,1,num=args.grid_size) #in units of stellar radius
     y = np.linspace(0,1,num=args.grid_size) #in units of stellar radius
 
+    #calculate the velocity grid
     vel_grid = calc_vel_stellar(x,y,args.stelinc,args.velStar,drr, pob)
-    print(vel_grid)
+    #print(vel_grid)
