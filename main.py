4d2c77dc5e5ec613678da2b1a01a5cbacdac0237
JVSeidel <julia.seidel@unige.ch>
2019-11-05 15:01:34 +0100

    tested velocity calculation

--- main.py
+++ main.py
@@ -36,18 +36,18 @@ def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate, proj_obliquity):
     alpha = convert_angle_to_rad(proj_obliquity)
     beta = convert_angle_to_rad(i_stellar)
     #pre calculate matrices
+    
     xy = np.einsum('i,j->ij',x,y)
     x_full = np.tile(x,(len(x),1))
     y_full = np.tile(y,(len(y),1)).T
     x_sq = x_full*x_full
     y_sq=y_full*y_full
-    
     #this is the z coordinate in the tilted coordinate system from Cegla+2016
     # with some trigonometry magic
-    z = np.sqrt(1.-x_sq-y_sq+2.*x_full*y_full*np.sin(2.*alpha))
-    #I have to add the stellar radius back in here somewhere
+    z = np.sqrt(1.-x_sq-y_sq+4.*xy*np.sin(alpha)*np.cos(alpha))
+
     #equation 8 from Cegla+2016
-    vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta*(1-diff_rot_rate*z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
+    vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
     
     return vel_stellar_grid
 
@@ -67,23 +67,21 @@ if __name__ == '__main__':
         help='Inclination of the star. type:float')
         parser.add_argument('orbinc', metavar='orbinc', type=float,
         help='Orbital inclination of the planet. type:float')
+        parser.add_argument('drr', metavar='drr', type=float,
+        help='Differential rotation rate in units of stellar radii. type:float')
+        parser.add_argument('pob', metavar='pob', type=float,
+        help='projected obliquity of the star in degrees. type:float')
         parser.add_argument('grid_size', metavar='grid', type=int, default=500,
         help='number of grid cells, default 500. type:int')
         args = parser.parse_args()
         test.test_parser(args)
     except ValueError as err:
         print("Parser: ",err.args)
-
-    #comment Julia: drr differential rotation rate, where do we get this from
-    #comment Julia: pob projected obliquity, where do we get this from
-    #for testing
-    drr = 1.
-    pob = 80.
     
     #two arrays for the x and y axis
     x = np.linspace(0,1,num=args.grid_size) #in units of stellar radius
     y = np.linspace(0,1,num=args.grid_size) #in units of stellar radius
 
     #calculate the velocity grid
-    vel_grid = calc_vel_stellar(x,y,args.stelinc,args.velStar,drr, pob)
-    #print(vel_grid)
+    vel_grid = calc_vel_stellar(x,y,args.stelinc,args.velStar,args.drr, args.pob)
+    print(vel_grid)
