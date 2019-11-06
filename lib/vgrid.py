22e278d02eb155456ee98cf86f7d02332096be04
Jens Hoeijmakers <jens.hoeijmakers@gmail.com>
2019-11-06 10:43:23 +0100

    Added output of flux grid in vgrid.py.

--- lib/vgrid.py
+++ lib/vgrid.py
@@ -32,3 +32,26 @@ def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate, proj_obliquity):
     vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
 
     return vel_stellar_grid
+
+
+def calc_flux_stellar(x,y,u1,u2):
+    import numpy as np
+    import lib.operations as ops
+
+    xy = np.einsum('i,j->ij',x,y)
+    x_full = np.tile(x,(len(x),1))
+    y_full = np.tile(y,(len(y),1)).T
+    x_sq = x_full*x_full
+    y_sq=y_full*y_full
+    #this is the z coordinate in the tilted coordinate system from Cegla+2016
+    # with some trigonometry magic
+    z = np.sqrt(1.-x_sq-y_sq)
+    flux_grid = z*0.0
+    for i in range(len(x)):
+        for j in range(len(y)):
+            if np.sqrt(x[i]**2+y[j]**2) <= 1.0:
+                flux_grid[j,i]=ops.limb_darkening((1.0-np.sqrt(x[i]**2+y[j]**2)),u1,u2)*(z[j,i]*0.0+1.0)#Last multiplication is to put NaNs back into place.
+
+    # plotting.plot_star_2D(x,y,mu_grid,cmap="hot",quantities=['','',''],units=['','',''],noshow=False)
+    flux_grid /= np.nansum(flux_grid)
+    return(flux_grid)
