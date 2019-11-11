3281c935b5ecb9fce7bc255cbd0da7a9d23f73fc
Jens Hoeijmakers <jens.hoeijmakers@gmail.com>
2019-11-11 10:14:00 +0100

    Ok I'm done making animations now.

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ import pdb
 import time
 import matplotlib.pyplot as pl
 import math
-import matplotlib.animation as animation
 #main body of code
 
 
@@ -53,10 +52,10 @@ if __name__ == '__main__':
 
     test.test_integrators()
 
-    T = 6000.0
+    T = 10000.0
     logg = 4.5
     u1 = 0.387
-    u1 = 0.0
+    u1 = 0.008
     u2 = 0.178
     u2 = 0.1
 
@@ -77,14 +76,30 @@ if __name__ == '__main__':
 
     #The following creates a transiting planet. We will need to offload it to some
     #tutorial-kind of script, but I put it here for now.
+    from matplotlib.patches import Circle
 
-    RpRs = 0.2
+
+    RpRs = 0.13
     nsteps = 100
-    xp = np.linspace(-2.0,2.0,nsteps)
-    yp = np.linspace(-0.5,0.2,nsteps)
+    xp1 = np.linspace(-1.5,1.5,nsteps)
+    yp1 = np.linspace(-0.5,0.2,nsteps)
+    xp2 = np.linspace(-0.1,-0.5,int(nsteps*1.0))
+    yp2 = np.linspace(2.5,-2.5,int(nsteps*1.0))
+    xp3 = np.linspace(1.3,0.0,nsteps)
+    yp3 = np.linspace(-0.5,1.3,nsteps)
+
+    xp = np.concatenate((xp1,xp2,xp3))
+    yp = np.concatenate((yp1,yp2,yp3))
     lightcurve = []
-    void1,void2,minflux,void3 = integrate.build_local_spectrum_fast(0,0,RpRs,wl,fx,args.wave_start,args.wave_end,x,y,vel_grid,flux_grid)
+    void1,void2,minflux,void3 = integrate.build_local_spectrum_fast(0,0,0.16,wl,fx,args.wave_start,args.wave_end,x,y,vel_grid,flux_grid)
     for i in range(len(xp)):
+        if i == nsteps-1:
+            RpRs = 0.1
+            lightcurve = []
+        if i == 2*nsteps-1:
+            lightcurve = []
+            RpRs = 0.16
+        # i=150
         wlp,Fp,flux,mask = integrate.build_local_spectrum_fast(xp[i],yp[i],RpRs,wl,fx,args.wave_start,args.wave_end,x,y,vel_grid,flux_grid)
         lightcurve.append(flux)
         fig,ax = pl.subplots(nrows=2, ncols=2,figsize=(8,8))
@@ -92,32 +107,48 @@ if __name__ == '__main__':
         ax[1][0].pcolormesh(x,y,vel_grid*mask,cmap='bwr')
         ax[0][0].axes.set_aspect('equal')
         ax[1][0].axes.set_aspect('equal')
+        planet1 = Circle((xp[i],yp[i]),RpRs, facecolor='black', edgecolor='black', lw=1)
+        planet2 = Circle((xp[i],yp[i]),RpRs, facecolor='black', edgecolor='black', lw=1)
+        ax[0][0].add_patch(planet1)
+        ax[1][0].add_patch(planet2)
+
 
         ax[0][1].plot(lightcurve,'.',color='black')
         ax[0][1].set_xlim((0,nsteps))
         ax[0][1].set_ylim((minflux-0.1*RpRs**2.0),1.0+0.1*RpRs**2)
         ax[1][1].plot(wlF,F/np.nanmax(F),color='black',alpha = 0.5)
+        ymin = np.nanmin(F/np.nanmax(F))
+        ymax = np.nanmax(F/np.nanmax(F))
+        linedepth = ymax - ymin
         ax[1][1].plot(wlF,(F-Fp)/np.nanmax(F-Fp),color='black')
-        ax[1][1].set_xlim((589.0,589.3))
+        ax[1][1].set_xlim((588.5,590.2))
+        ax[1][1].set_ylim((ymin-0.1*linedepth,ymax+0.3*linedepth))
+        ax2 = ax[1][1].twinx()
+        ax2.plot(wlF,(F-Fp)*np.nanmax(F)/F/np.nanmax(F-Fp),color='skyblue')
+        ax2.set_ylim((1.0-(1-ymin-0.1*linedepth)/2,1.0+(ymax+0.3*linedepth-1)/2))
+        ax2.set_ylabel('Ratio F_in/F_out',fontsize = 7)
+        ax2.tick_params(axis='both', which='major', labelsize=6)
+        ax2.tick_params(axis='both', which='minor', labelsize=5)
 
         ax[0][0].set_ylabel('Y (Rs)',fontsize=7)
-        ax[0][0].tick_params(axis='both', which='major', labelsize=8)
-        ax[0][0].tick_params(axis='both', which='minor', labelsize=6)
+        ax[0][0].tick_params(axis='both', which='major', labelsize=6)
+        ax[0][0].tick_params(axis='both', which='minor', labelsize=5)
 
         ax[1][0].set_ylabel('Y (Rs)',fontsize=7)
         ax[1][0].set_xlabel('X (Rs)',fontsize=7)
-        ax[1][0].tick_params(axis='both', which='major', labelsize=8)
-        ax[1][0].tick_params(axis='both', which='minor', labelsize=6)
+        ax[1][0].tick_params(axis='both', which='major', labelsize=6)
+        ax[1][0].tick_params(axis='both', which='minor', labelsize=5)
+
 
         ax[0][1].set_ylabel('Normalised flux',fontsize=7)
         ax[0][1].set_xlabel('Timestep',fontsize='small')
-        ax[0][1].tick_params(axis='both', which='major', labelsize=8)
-        ax[0][1].tick_params(axis='both', which='minor', labelsize=6)
+        ax[0][1].tick_params(axis='both', which='major', labelsize=6)
+        ax[0][1].tick_params(axis='both', which='minor', labelsize=5)
 
         ax[1][1].set_ylabel('Normalised flux',fontsize=7)
         ax[1][1].set_xlabel('Wavelength (nm)',fontsize=7)
-        ax[1][1].tick_params(axis='both', which='major', labelsize=8)
-        ax[1][1].tick_params(axis='both', which='minor', labelsize=6)
+        ax[1][1].tick_params(axis='both', which='major', labelsize=6)
+        ax[1][1].tick_params(axis='both', which='minor', labelsize=5)
         # pl.show()
         # sys.exit()
         if len(str(i)) == 1:
@@ -126,4 +157,8 @@ if __name__ == '__main__':
             out = '00'+str(i)
         if len(str(i)) == 3:
             out = '0'+str(i)
+        if len(str(i)) == 4:
+            out = str(i)
         fig.savefig('anim/'+out+'.png', dpi=fig.dpi)
+        integrate.statusbar(i,xp)
+        pl.close()
