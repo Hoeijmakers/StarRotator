5a11889f8cb0cb9ade883edaa3c963cafee112a4
Jens Hoeijmakers <jens.hoeijmakers@fysik.lu.se>
2025-07-15 15:13:31 +0200

    Refactor 2D vgrid calculation with jax.

--- starrotator/lib/vgrid.py
+++ starrotator/lib/vgrid.py
@@ -1,79 +1,139 @@
-def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate, proj_obliquity):
+import jax
+import jax.numpy as jnp
+from jax import jit
+import numpy as np
+
+@jit
+def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate):
     """
-    based on Cegla+2016. See Fig. 3 and equations 2 - 8
-    https://arxiv.org/pdf/1602.00322.pdf
-    It takes the stellar parameters and then calculates the stellar velocities in all bins for one quarter of the stellar disk
-    input: x, y: 1D numpy arrays to create the stellar grid in units of stellar radius
-           i_stellar: inclination in degrees
-           vel_eq: equatorial stellar velocity
-           diff_rot_rate: differential rotation rate
-           proj_obliquity: projected obliquity
-    output: vel_stellar_grid: 2D numpy array of stellar velocities over one quarter of the stellar disk
+    This calculates the velocity grid of a rotating star.
+    based on Cegla+2016. See Fig. 3 and equations 2 - 8 of [1].
+
+    It takes the stellar parameters and then calculates the stellar velocities on a pixellated stellar disk.
+
+    Parameters
+    ----------
+        x : array-like
+            1D numpy arrays to create the stellar grid in units of stellar radius in the x-direction.
+
+        y : array-like
+            1D numpy arrays to create the stellar grid in units of stellar radius in the y-direction.
+
+        i_stellar : float
+            Inclination of the stellar spin axis in degrees.
+
+        vel_eq : float
+            Equatorial velocity of the star in km/s.
+            
+        diff_rot_rate : float
+            Differential rotation parameter as defined in Cegla et al. 2016.
+            If you wish to use a non-differentially rotating star, you'll be better off calculating 
+            the stellar spectrum without using this fully pixellated model.
+
+    Returns
+    -------
+        vel_stellar_grid : array-like
+            A 2D, pixellated model of the stellar disk mapping the radial velocity as a function of 
+            projected x and y position, in km/s.
+
+    References
+    ----------
+    .. [1] Cegla, Lovis, et al. (2016), A&A, 588, A127
+        (https://arxiv.org/pdf/1602.00322.pdf)        
     """
-    import numpy as np
-    import pdb
-    import lib.test as test
+    #Convert angles to rad
+    # alpha = jnp.radians(proj_obliquity)
+    beta = jnp.radians(i_stellar)
 
-    #Standard tests on input
-    test.typetest(x,np.ndarray,varname='x in calc_vel_stellar')
-    test.typetest(y,np.ndarray,varname='x in calc_vel_stellar')
-    test.typetest(i_stellar,float,varname='i_stellar in calc_vel_stellar')
-    test.typetest(vel_eq,float,varname='vel_eq in calc_vel_stellar')
-    test.typetest(diff_rot_rate,float,varname='diff_rot_rate in calc_vel_stellar')
-    test.typetest(proj_obliquity,float,varname='proj_obliquity in calc_vel_stellar')
-    test.nantest(x,varname='x in calc_vel_stellar')
-    test.nantest(y,varname='y in calc_vel_stellar')
-    test.nantest(i_stellar,varname='i_stellar in calc_vel_stellar')
-    test.nantest(vel_eq,varname='vel_eq in calc_vel_stellar')
-    test.nantest(diff_rot_rate,varname='diff_rot_rate in calc_vel_stellar')
-    test.nantest(proj_obliquity,varname='proj_obliquity in calc_vel_stellar')
-    test.notnegativetest(vel_eq,varname='vel_eq in calc_vel_stellar')
+    z,x_full,y_full = calc_z(x,y)#This is jitted. x_full and y_full are simple coordinate arrays.
 
-    #Careful! all angles have to be in radians!
-    #Think carefully about which ones of these have to be transposed
+    # Doing equation 8 from Cegla+2016 would go like this:
+    # vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
 
-    #convert angles to rad
-    alpha = np.radians(proj_obliquity)
-    beta = np.radians(i_stellar)
-    #pre calculate matrices
-    z,x_full,y_full = calc_z(x,y)
-    #equation 8 from Cegla+2016
-#    vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
-
-     #this has the star aligned to the coordinate system. Like this the projected obliquity is not incorporated here. The tilt of the star compared to the planet has to be incorporated in the planet's path.
-    vel_stellar_grid = x_full*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*y_full))
-
+     # However, we align the star to the coordinate system, and so the projected obliquity is not incorporated here. 
+     # The tilt of the star compared to the planet has to be incorporated in the planet's path. So we do one rotation
+     # fewer than Cegla 2016 and we don't align the x-y grid to the orbital plane.
+    vel_stellar_grid = x_full*vel_eq*jnp.sin(beta)*(1.-diff_rot_rate*(z*jnp.sin(jnp.pi/2.-beta)+jnp.cos(jnp.pi/2.-beta)*y_full))
     return(vel_stellar_grid)
 
+
+
+def calc_vel_stellar_old(x,y,i_stellar, vel_eq, diff_rot_rate):
+    """
+    Depricated version of the above.
+    """
+    # Convert angles to rad
+    # alpha = np.radians(proj_obliquity)
+    beta = np.radians(i_stellar)
+    #pre calculate matrices
+    z,x_full,y_full = calc_z(x,y)#This is jitted.
+
+    # Doing equation 8 from Cegla+2016 would go like this:
+    # vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
+
+     # However, we align the star to the coordinate system, and so the projected obliquity is not incorporated here. 
+     # The tilt of the star compared to the planet has to be incorporated in the planet's path. So we do one rotation
+     # fewer than Cegla 2016 and we don't align the x-y grid to the orbital plane.
+    vel_stellar_grid = x_full*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*y_full))
+    return(vel_stellar_grid)
+
+
+
+
+@jit
 def calc_z(x,y):
-    """This is a nice little wrapper to calculate the distance matrix of a circle."""
-    import numpy as np
-    xy = np.einsum('i,j->ij',x,y)
-    x_full = np.tile(x,(len(y),1))
-    y_full = np.tile(y,(len(x),1)).T
-    x_sq = x_full*x_full
-    y_sq=y_full*y_full
-    #this is the z coordinate in the tilted coordinate system from Cegla+2016
-    # with some trigonometry magic
-    #it's also the z coordinate in the untilted system for similarity reasons
-    d = 1.-x_sq-y_sq
-    d[d<0]=np.nan#This supresses a runtime warning.
-    return(np.sqrt(d),x_full,y_full)
+    """
+    This calculates the z-coordinate of the star as used in [1]_. See Fig. 3 and equations 2 - 8.
+    It takes the 1D x and y grids.
+
+    Parameters
+    ----------
+        x : array
+            1D numpy arrays to create the stellar grid in units of stellar radius in the x-direction.
+
+        y : array
+            1D numpy arrays to create the stellar grid in units of stellar radius in the y-direction.
+
+        i_stellar : float
+            Inclination of the stellar spin axis in degrees.
+
+        vel_eq : float
+            Equatorial velocity of the star in km/s.
+            
+        diff_rot_rate : float
+            Differential rotation parameter as defined in Cegla et al. 2016.
+            If you wish to use a non-differentially rotating star, you'll be better off calculating 
+            the stellar spectrum without using this fully pixellated model.
+
+    Returns
+    -------
+        d : array-like
+            A 2D, pixellated model of the stellar disk mapping the z coordinate as used in [1]_, in
+            units of x and y.
+
+        x_full : array-like
+            The x-coordinate broadcasted along the entire 2D grid.
+
+        y_full : array-like
+            The y-coordinate broadcasted along the entire 2D grid.
+
+    References
+    ----------
+    .. [1] Cegla, Lovis, et al. (2016), A&A, 588, A127
+        (https://arxiv.org/pdf/1602.00322.pdf)  
+    """
+    x_full,y_full = x[:, None],y[None, :]
+    d = 1 - x_full**2 - y_full**2
+    d_clipped = jnp.where(d < 0,jnp.nan,d)
+    return(jnp.sqrt(d_clipped),x_full,y_full)
 
 
+
+# CONTINUE HERE NEXT TIME. THIS CAN BE VASTLY SPED UP.
 def calc_flux_stellar(x,y,u1,u2):
     import numpy as np
     import lib.operations as ops
-    import lib.test as test
 
-    test.typetest(x,np.ndarray,varname='x in calc_flux_stellar')
-    test.typetest(y,np.ndarray,varname='x in calc_flux_stellar')
-    test.typetest(u1,[int,float],varname='i_stellar in calc_flux_stellar')
-    test.typetest(u2,[int,float],varname='vel_eq in calc_flux_stellar')
-    test.nantest(x,varname='x in calc_flux_stellar')
-    test.nantest(y,varname='y in calc_flux_stellar')
-    test.nantest(u1,varname='x in calc_flux_stellar')
-    test.nantest(u2,varname='y in calc_flux_stellar')
 
     z,x_full,y_full = calc_z(x,y)
     flux_grid = z*0.0
