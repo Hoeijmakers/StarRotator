a62b64b4bab6fe87c1a8ecb79e20b8c488971832
Jens Hoeijmakers <jens.hoeijmakers@fysik.lu.se>
2025-07-17 12:13:37 +0200

    Fix orientation of grid (swap x,y) and fix error in DRR equation

--- starrotator/lib/vgrid.py
+++ starrotator/lib/vgrid.py
@@ -12,7 +12,7 @@ from functools import partial
 def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate):
     """
     This calculates the velocity grid of a rotating star.
-    based on Cegla+2016. See Fig. 3 and equations 2 - 8 of [1].
+    based on Cegla+2016. See Fig. 3 and equations 2 - 8 of [1]_.
 
     It takes the stellar parameters and then calculates the stellar velocities on a pixellated stellar disk.
 
@@ -31,9 +31,9 @@ def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate):
             Equatorial velocity of the star in km/s.
             
         diff_rot_rate : float
-            Differential rotation parameter as defined in Cegla et al. 2016.
+            Differential rotation parameter alpha as defined in Cegla et al. 2016.
             If you wish to use a non-differentially rotating star, you'll be better off calculating 
-            the stellar spectrum without using this fully pixellated model.
+            the stellar spectrum without using this fully pixellated model (integration v1).
 
     Returns
     -------
@@ -46,26 +46,27 @@ def calc_vel_stellar(x,y,i_stellar, vel_eq, diff_rot_rate):
     .. [1] Cegla, Lovis, et al. (2016), A&A, 588, A127
         (https://arxiv.org/pdf/1602.00322.pdf)        
     """
-    #Convert angles to rad
-    # alpha = jnp.radians(proj_obliquity)
-    beta = jnp.radians(i_stellar)
-
     z,x_full,y_full = calc_z(x,y)#This is jitted. x_full and y_full are simple coordinate arrays.
 
-    # Doing equation 8 from Cegla+2016 would go like this:
-    # vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*(x_full*np.sin(alpha)-y_full*np.cos(alpha))))
+    # Doing equation 8 from Cegla 2016 would go like this:
+    # vel_stellar_grid = (x_full*np.cos(alpha)-y_full*np.sin(alpha))*...
+    # vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*...
+    # (x_full*np.sin(alpha)-y_full*np.cos(alpha))))
 
      # However, we align the star to the coordinate system, and so the projected obliquity is not incorporated here. 
      # The tilt of the star compared to the planet has to be incorporated in the planet's path. So we do one rotation
      # fewer than Cegla 2016 and we don't align the x-y grid to the orbital plane.
-    vel_stellar_grid = x_full*vel_eq*jnp.sin(beta)*(1.-diff_rot_rate*(z*jnp.sin(jnp.pi/2.-beta)+jnp.cos(jnp.pi/2.-beta)*y_full))
+    i_rad = jnp.radians(i_stellar)
+    beta = jnp.pi/2 - i_rad # In the paragraph preceding eq 6 in C2016.
+    y_per_prime = (z*jnp.sin(beta)+jnp.cos(beta)*y_full)# Equation 7 in C2016.
+    vel_stellar_grid = x_full*vel_eq*jnp.sin(i_rad) *  (1.0 - diff_rot_rate * y_per_prime**2) # Equation 8 in C2016.
     return(vel_stellar_grid)
 
 
 
 def calc_vel_stellar_old(x,y,i_stellar, vel_eq, diff_rot_rate):
     """
-    Depricated version of the above.
+    Depricated version of the above. Note that there was a missing square in the line that activated drr!!
     """
     # Convert angles to rad
     # alpha = np.radians(proj_obliquity)
@@ -79,7 +80,7 @@ def calc_vel_stellar_old(x,y,i_stellar, vel_eq, diff_rot_rate):
      # However, we align the star to the coordinate system, and so the projected obliquity is not incorporated here. 
      # The tilt of the star compared to the planet has to be incorporated in the planet's path. So we do one rotation
      # fewer than Cegla 2016 and we don't align the x-y grid to the orbital plane.
-    vel_stellar_grid = x_full*vel_eq*np.sin(beta)*(1.-diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*y_full))
+    vel_stellar_grid = x_full*vel_eq*np.sin(beta) * (1.0 - diff_rot_rate*(z*np.sin(np.pi/2.-beta)+np.cos(np.pi/2.-beta)*y_full))
     return(vel_stellar_grid)
 
 
@@ -127,7 +128,7 @@ def calc_z(x,y):
     .. [1] Cegla, Lovis, et al. (2016), A&A, 588, A127
         (https://arxiv.org/pdf/1602.00322.pdf)  
     """
-    x_full,y_full = x[:, None],y[None, :]
+    x_full,y_full = x[None, :],y[:, None]
     d = 1 - x_full**2 - y_full**2
     d_clipped = jnp.where(d < 0,jnp.nan,d)
     return(jnp.sqrt(d_clipped),x_full,y_full)
@@ -169,7 +170,7 @@ def calc_flux_stellar(x,y,u1,u2,norm=True):
 
     # The limb darkening function below is jitted. And x,y are passed such that they broadcast
     # into a 2D array.
-    flux_grid = ops.limb_darkening(jnp.sqrt(x[:, None]**2 + y[None, :]**2),u1,u2)
+    flux_grid = ops.limb_darkening(jnp.sqrt(x[None, :]**2 + y[:,None]**2),u1,u2)
     if norm:
         return(flux_grid/jnp.nansum(flux_grid))
     else:
