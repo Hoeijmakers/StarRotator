6019a8df8b83f8428673347002688d8fecdf847f
Jens Hoeijmakers <jens.hoeijmakers@fysik.lu.se>
2025-09-21 22:57:22 +0200

    Wokring v of sum_hidden_spectrum_v1_mu -- need testing

--- starrotator/lib/integrate.py
+++ starrotator/lib/integrate.py
@@ -67,11 +67,6 @@ def sum_stellar_spectrum_v1(wl,fx,vel_eq,i_stellar,a1,a2,N=400,norm=False):
 
 
 
-#NOTE THAT THIS IS STILL POORLY DESIGNED. WL_ARRAY, FX_ARRAY, AND MU SHOULD REALISTICALLY NOT BE TRACED 
-#VARIABLES! Though maybe it doesn't hurt if they are...
-
-#I verified that the following returns the same answer (pi) when inputting a spectrum that is flat at 1.0, and independent of mu,
-#as sum_stellar_spectrum_v1 with limb darkening equal to 0. 
 @partial(jax.jit,static_argnames=["N"])
 def sum_stellar_spectrum_v1_mu(wl,fx_array,vel_eq,i_stellar,mu,N=400):
     """This builds the stellar spectrum by doppler-shifting and interpolating an array of input
@@ -93,6 +88,7 @@ def sum_stellar_spectrum_v1_mu(wl,fx_array,vel_eq,i_stellar,mu,N=400):
 
         fx_array : array-like
             Two-dimensional array of the stellar model fluxes corresponding to wl, at different mu angles.
+            If you intend to pass only one mu angle for some reason, then it should be passed explicitly with a leading dimension.
 
         vel_eq : float
             The equatorial velocity of the star in km/s.
@@ -100,7 +96,7 @@ def sum_stellar_spectrum_v1_mu(wl,fx_array,vel_eq,i_stellar,mu,N=400):
         i_stellar : float
             The inclination of the stellar spin axis in degrees. 0 is pole-on.
 
-        mus : array-like
+        mu : array-like
             The array of mu-values corresponding to the array of spectra.
 
         norm : bool
@@ -117,8 +113,10 @@ def sum_stellar_spectrum_v1_mu(wl,fx_array,vel_eq,i_stellar,mu,N=400):
         -----
         In this function, the input spectra are broadcast to a shape of n_wl times N times n_mu_angles.
         If you have 1e5 wavelength points, 400 gridpoints and 10 mu angles, that's 3.6 GB.
-        So do not choose too many mu angles or wavelength points, or you will overflow your RAM, and there
-        is no explicit guard against that.
+        So do not choose too many mu angles or many wavelength points at the same time, or you will 
+        overflow your RAM, and there is no explicit guard against that.
+        If a real-life application occurs where you're overflowing your RAM, you could always batch the
+        computation in wavelength manually.
 
     """
     x = jnp.linspace(-1,1,N)
@@ -126,11 +124,20 @@ def sum_stellar_spectrum_v1_mu(wl,fx_array,vel_eq,i_stellar,mu,N=400):
     v_axis = x*vel_eq*jnp.sin(jnp.radians(i_stellar)) # velocities along the y=0 axis.
 
 
-    doppler_shift_batch = jax.vmap(doppler_shift, in_axes=(None, 0, None))
+    doppler_shift_batch = jax.vmap(doppler_shift, in_axes=(None, 0, None)) # Here is some of the magic.
+    #This creates a function doppler_shift_batch that is a vectorised execution of the doppler_shift
+    #function, looping over axis 0 of the second input, which is fx_array. So each row of fx_array
+    #gets doppler shifted by v_axis.
 
     fx_array_shifted = doppler_shift_batch(wl,fx_array,v_axis)
-    #Note that if this function is called repeatedly with different shapes, this will trigger jax recompilations. 
+    #So the output will have dimensions len(v) * len(fx_array) * len(wl) whereas the shape of the output
+    # of a single call to doppler_shift would have dimensions len(v)*len(wl)).
+
+    #Note that if any jitted function is called repeatedly with different shapes for input, this will trigger jax recompilations. 
     #Also note that the output can be big: For 100,000 wavelength points, 200 velocities and 10 mu angles, this could be 1.6GB in memory.
+
+
+
 
     #Now follows the main logic of this computation.
     #We create a map of bins of mu across the stellar disk.
@@ -294,7 +301,8 @@ def sum_hidden_spectrum_v1(wl,fx,xp,yp,Rp,vel_eq,i_stellar,a1,a2,N=100):
         N : int
             Number of grid-points in the x direction onto which the planet is simulated. Note that the resolution of the planet is independent
             of the resolution of the grid of the star. Note that as this calculation is batched over planet positions, cases where
-            you have many planet positions and high N will result in high memory load if batched is set to false.
+            you have many planet positions and high N will result in high memory load. In case of overflowing RAM, you can always
+            chunck the computation in wavelength yourself.
 
         Returns
         -------
@@ -305,20 +313,174 @@ def sum_hidden_spectrum_v1(wl,fx,xp,yp,Rp,vel_eq,i_stellar,a1,a2,N=100):
             disk, that depends only on a1 and a2. Note that as part of this process, the flux is multiplied by the
             differential dx*R, so that the integral is insensitive to N.
     """
-    x = jnp.linspace(-1,1,N)
-    dxR = (x[1]-x[0])*Rp
-    x_array = x*Rp + xp[:,None]
-    y_max = yp[:,None] + jnp.sqrt(1-x**2)*Rp
-    y_min = yp[:,None] - jnp.sqrt(1-x**2)*Rp
-    y_star_max = jnp.sqrt(1-x_array**2)*0.999999
+    x = jnp.linspace(-1,1,N) #This defines the grid over which the integration takes place.
+    #The strategy here is to calculate (integrate) the amount of surface of the stellar disk
+    #in the vertical direction, for each x-gridpoint. This is done analytically for a
+    #limb darkened disk, but with y-limits that are different from the simple 1=x*2+y*2 if you are calculating
+    #this integral on the full disk (sum_stellar_spectrum_v1). Instead, we do the same thing but just
+    #evaluating the definite integral.
+    #Everything below is there to calculate the y-limits of the PLANET disk, or the part of the planet disk that
+    #is in front of the stellar disk (ignoring the part that is outside of the disk at in/egress).
+    dxR = (x[1]-x[0])*Rp #Our grid is not from -1,1, but from -Rp to Rp. So much finer grid points than the star, for the same N.
+    x_array = x*Rp + xp[:,None]#Shifting to the location of the planet.
+    y_max = yp[:,None] + jnp.sqrt(1-x**2)*Rp #This is the edge of the planet on the top.
+    y_min = yp[:,None] - jnp.sqrt(1-x**2)*Rp #The lower edge of the planet.
+    y_star_max = jnp.sqrt(1-x_array**2)*0.999999#The maximum y you can have is the edge of the stellar disk.
     y_star_min = -jnp.sqrt(1-x_array**2)*0.999999
-    y_final_max = jnp.clip(y_max,y_star_min,y_star_max)
-    y_final_min = jnp.clip(y_min,y_star_min,y_star_max)
-    v_axes = x_array*vel_eq*jnp.sin(jnp.radians(i_stellar)) # velocities along the y=0 axis.
-    weights = vert_int_q_ld_bounded(x_array,y_final_min,y_final_max,a1,a2)
-    fx_shifted = doppler_shift(wl,fx,v_axes).T*weights # /jnp.nansum(weights)
+    y_final_max = jnp.clip(y_max,y_star_min,y_star_max)#Which is smaller? The y-coord of the planet or the y-coord of the stellar disk?
+    y_final_min = jnp.clip(y_min,y_star_min,y_star_max)#jnp.clip is so awesome.
+    v_axes = x_array*vel_eq*jnp.sin(jnp.radians(i_stellar)) # velocities along the y=0 axis. No drr so this is nicely uniform along y.
+    weights = vert_int_q_ld_bounded(x_array,y_final_min,y_final_max,a1,a2) #This here is all the magic, the analytical integration.
+    #Weights now has the same shape as x_array. x_array could be a 2D array, N_planet_phases x N_gridpoints.
+    #For each planet phase, weights expresses the amount of surface area (from y_min to y_max) at each x-position (meaning: at each RV).
+    #Thats why a dxR appears below, that is the differential.
+    fx_shifted = doppler_shift(wl,fx,v_axes).T*weights # This is a very big array isn't it? For large N this is large too?
     F_out = jnp.nansum(fx_shifted,axis=2).T * dxR #Multiply with the differential for normalization.
     return(F_out)
+
+
+
+
+@jit
+def find_mu_interval(mu_array, mu_value):
+    # Find the index of the right boundary
+    l2 = jnp.searchsorted(mu_array, mu_value, side="right")
+    # Left boundary is one index before
+    l1 = jnp.maximum(l2 - 1, 0)
+    # Clip right_idx to avoid going out of bounds
+    l2 = jnp.minimum(l2, mu_array.shape[0] - 1)
+    return(l1, l2)
+
+@jit
+def mu_interp_weight(mu_array,l1,l2,mu_value):
+    # Get interval endpoints
+    mu_left = mu_array[l1]
+    mu_right = mu_array[l2]
+    # Compute fractional distance, safe against divide-by-zero
+    t = (mu_value - mu_left) / (mu_right - mu_left + 1e-12)
+    t = jnp.clip(t, 0.0, 1.0)  # avoid going outside [0,1]  
+    return(t)
+
+@jit
+def interp_fx_array(mu_array,fx_array,mu_value):
+    """
+    This linearly interpolates the disk-resolved spectrum
+    given a value of mu.
+    It handles edge cases (so if the target value is outside the range
+    of the my values provided, it edge clips as it should).
+    It also allows for broadcasting, so you can get many
+    interpolated spectra returned. Note that this will cause a
+    jax recompile (shape change).
+    """
+    l1,l2 = find_mu_interval(mu_array,mu_value)
+    t = mu_interp_weight(mu_array,l1,l2,mu_value)
+    fx_interpolated = (fx_array[l1].T*(1-t) + t*fx_array[l2].T).T
+    return(fx_interpolated)
+    
+
+
+
+@partial(jax.jit,static_argnames=['N','small_planet'])
+def sum_hidden_spectrum_v1_mu(wl,fx_array,xp,yp,Rp,vel_eq,i_stellar,mu,N=100,small_planet=True):
+    """This builds the stellar spectrum that is hidden behind the planet by doppler-shifting 
+        and interpolating the input spectrum, summing only over the range of coordinates that is
+        obscured by the planet disk.
+
+        This version of the integration assumes a SIMPLE VELOCITY GRID (that means: without drr)
+        and a MU-DEPENDENT (mu-resolved) stellar spectrum. Center-to-limb variation is thus
+        modelled by the variation in the limb-resolved spectra. 
+
+        This function integrates in a manner that is equivalent to sum_stellar_spectrum_v1_mu,
+        unless the small-planet approximation is made, in which case it integrates like sum_hidden_spectrum_v1.
+
+
+        Parameters
+        ----------
+        wl : array-like
+            The stellar model wavelengths.
+
+        fx_array : array-like
+            Two-dimensional array of the stellar model fluxes corresponding to wl, at different mu angles.
+            If you intend to pass only one mu angle for some reason, then it should be passed explicitly with a leading dimension.
+
+        xp : array-like
+            The horizontal location of the planet, either as a float or as a 1D array.
+
+        yp : array-like
+            The vertical location of the planet, either as a float or as a 1D array.
+
+        Rp : float
+            The radois of the planet in units of stellar radii.
+
+        vel_eq : float
+            The equatorial velocity of the star in km/s.
+
+        i_stellar : float
+            The inclination of the stellar spin axis in degrees.
+
+        mu : array-like
+            The array of mu-values corresponding to the array of spectra.
+            
+
+        N : int
+            Number of grid-points in the x direction onto which the planet is simulated. Note that the resolution of the planet is independent
+            of the resolution of the grid of the star. Note that as this calculation is batched over planet positions, cases where
+            you have many planet positions and high N will result in high memory load if batched is set to false.
+
+        small_planet : bool
+            Make a small-planet approximation (true) or not (false). In the small-planet approximation, the mu-variation across the planetary disk is neglected, and a single mu
+            angle (at planet center) is assumed. The spectrum is interpolated from the two nearest mu angles for which spectra are provided.
+
+        Returns
+        -------
+        F : array
+            The flux axis of the summed spectrum corresponding to the input wavelength points, for each planet position
+            (so this is a 2D array as xp,yp are 1D). This integrates to an arbitrary number, but this scaling corresponds
+            to the scaling of the full disk. Normalisation is therefore achieved by dividing by the integral of the full
+            disk, that depends only on a1 and a2. Note that as part of this process, the flux is multiplied by the
+            differential dx*R, so that the integral is insensitive to N.
+    """
+
+    if small_planet:
+        x = jnp.linspace(-1,1,N)#This whole strategy is the same as sum_hidden_spectrum_v1 above.
+        #We can even compute the weights in the same way. The only thing we need to do differently
+        #is determine which fx_spectrum is associated with which which mu angle.
+        dxR = (x[1]-x[0])*Rp
+        x_array = x*Rp + xp[:,None]
+        y_max = yp[:,None] + jnp.sqrt(1-x**2)*Rp
+        y_min = yp[:,None] - jnp.sqrt(1-x**2)*Rp
+        y_star_max = jnp.sqrt(1-x_array**2)*0.999999
+        y_star_min = -jnp.sqrt(1-x_array**2)*0.999999
+        y_final_max = jnp.clip(y_max,y_star_min,y_star_max)
+        y_final_min = jnp.clip(y_min,y_star_min,y_star_max)
+        v_axes = x_array*vel_eq*jnp.sin(jnp.radians(i_stellar)) # velocities along the y=0 axis. Same shape as x_array.
+        weights = vert_int_q_ld_bounded(x_array,y_final_min,y_final_max,0.0,0.0) #No limb darkening since we are 
+        #dealing with mu-dependent spectra.
+
+        # Continue here by calculating the mu's corresponding to the xp,yp positions.
+        mup = jnp.sqrt(xp**2+yp**2)
+        # Create an array of fx's that match that (by interpolating what the stellar spectrum)
+        # is at the mu of each planet location.
+        fx_p = interp_fx_array(mu,fx_array,mup) # These are the interpolated planet spectra belonging to each xp,yp.
+
+
+        doppler_shift_batch = jax.vmap(doppler_shift, in_axes=(None, 0, 0)) # Here is some of the magic.
+        # #This creates a function doppler_shift_batch that is a vectorised execution of the doppler_shift
+        # #function, looping over axis 0 of the second input, which is fx_array. So each row of fx_array
+        # #gets doppler shifted by v_axis.
+
+
+
+        fx_shifted = doppler_shift_batch(wl,fx_p,v_axes).T * weights.T
+        F_out = jnp.nansum(fx_shifted,axis=1).T * dxR
+        return(F_out)
+    else:
+        raise Exception("NOT-SMALL PLANET IS NOT IMPLEMENTED YET.")
+
+
+
+
+
 
 
 
